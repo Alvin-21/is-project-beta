b13db9086d11a6c0ca2e3fb4c6ea0d31aa89c5d0
Alvin <alvinawuor8@gmail.com>
2022-07-06 08:29:47 +0300

    Revert process.php back to default.

--- process.php
+++ process.php
@@ -72,19 +72,10 @@ if (isset($_POST['login_user'])) {
   	$query = "SELECT * FROM cms1_user WHERE username='$username' AND password='$password'";
   	$results = mysqli_query($db, $query);
   	if (mysqli_num_rows($results) == 1) {
-      $logged_in_user = mysqli_fetch_assoc($results);
-			if ($logged_in_user['user_type'] == '1') {
-
-				$_SESSION['username'] = $username;
-				$_SESSION['success']  = "You are now logged in";
-				header('location: index.php');		  
-			} else{
-				$_SESSION['username'] = $username;
-				$_SESSION['success']  = "You are now logged in";
-
-				header('location: register.php');
-			}
-  	} else {
+  	  $_SESSION['username'] = $username;
+  	  $_SESSION['success'] = "You are now logged in.";
+  	  header('location: index.php');
+  	}else {
   		array_push($errors, "Wrong username/password");
   	}
   }
